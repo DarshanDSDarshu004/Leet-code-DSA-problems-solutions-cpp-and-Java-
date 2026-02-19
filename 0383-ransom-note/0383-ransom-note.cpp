a85eacb2b95989990462865479d66065d4fc4b78
DarshanDSDarshu004 <ds.darshan004@gmail.com>
2026-02-19 23:40:50 +0530

    Time: 0 ms (100%), Space: 11.8 MB (49.5%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -3,21 +3,15 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         int n1=ransomNote.size();
         int n2=magazine.size();
-        vector<int>freq1(26,0),freq2(26,0);
-
-        for(int i=0;i<n2;i++){
-            freq2[magazine[i]-'a']++;
+        vector<int>freq(26,0);
+        for(char c:magazine){
+            freq[c-'a']++;
         }
-         for(int i=0;i<n1;i++){
-            freq1[ransomNote[i]-'a']++;
-        }
-
-        for(int i=0;i<n1;i++){
-            if(!(freq2[ransomNote[i]-'a']>=freq1[ransomNote[i]-'a']))
+         for(char c:ransomNote){
+            freq[c-'a']--;
+            if(freq[c-'a']<0)
             return false;
         }
         return true;
-        
-        
     }
 };
